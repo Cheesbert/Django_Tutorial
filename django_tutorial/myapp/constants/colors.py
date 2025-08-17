1025a01f4c53c804f00c197810a957b46600b1f1
Cheesbert <61742325+Cheesbert@users.noreply.github.com>
2025-08-17 21:10:05 +0200

    changed color to enum with color types

--- django_tutorial/myapp/constants/colors.py
+++ django_tutorial/myapp/constants/colors.py
@@ -1,14 +1,28 @@
+from enum import Enum
 
+class Color(Enum):
+    BLACK   = (0, 0, 0)
+    WHITE   = (255, 255, 255)
+    GRAY    = (128, 128, 128)
 
-BLACK = 0
-WHITE = (255, 255, 255)
-GRAY = 128
+    RED     = (255, 0, 0)
+    GREEN   = (0, 255, 0)
+    BLUE    = (0, 0, 255)
+    YELLOW  = (255, 255, 0)
+    CYAN    = (0, 255, 255)
+    MAGENTA = (255, 0, 255)
+    ORANGE  = (255, 165, 0)
+    PURPLE  = (128, 0, 128)
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
-ORANGE = (255, 165, 0)
-PURPLE = (128, 0, 128)
+    def rgb(self) -> tuple[int, int, int]:
+        """Return as RGB tuple for OpenCV / NumPy / PIL."""
+        return self.value
+
+    def bgr(self) -> tuple[int, int, int]:
+        """Return as BGR tuple (OpenCV default)."""
+        r, g, b = self.value
+        return (b, g, r)
+
+    def hex(self) -> str:
+        """Return as HEX string for HTML/CSS."""
+        return "#{:02x}{:02x}{:02x}".format(*self.value)
